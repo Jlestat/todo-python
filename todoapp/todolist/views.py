3b3f4a8c85de25aaa7694e1fb6bc12b96504b236
Jlestat <jlestatt@gmail.com>
2024-01-17 23:02:12 +0300

    add views and templates

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import ToDo
+
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello')
+    todos = ToDo.objects.all()
+    return render(request, 'todoapp/index.html', {'todo_list': todos, 'title': 'Главная страница'})
