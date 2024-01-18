8d57540a4eee8817e08228fb9ae53bce0999d44e
Jlestat <jlestatt@gmail.com>
2024-01-18 21:28:58 +0300

    creating routes

--- todoapp/todolist/urls.py
+++ todoapp/todolist/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('add', add, name='add'),
+    path('update/<int:todo_id>/', update, name='update'),
+    path('delete/<int:todo_id>/', delete, name='delete')
 ]
